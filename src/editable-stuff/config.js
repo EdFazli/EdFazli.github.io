ad1f6c7302ba2a864b0e9f285371eae8974c0b6f
Syed Ahmad Fazli <fazli.ismail@alibaba-inc.com>
2024-07-24 10:00:30 +0800

    adding experiences

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -100,23 +100,19 @@ const skills = {
   show: true,
   heading: "Skills",
   hardSkills: [
-    { name: "Python", value: 90 },
-    { name: "SQL", value: 75 },
-    { name: "Data Structures", value: 85 },
-    { name: "C/C++", value: 65 },
-    { name: "JavaScript", value: 90 },
-    { name: "React", value: 65 },
-    { name: "HTML/CSS", value: 55 },
-    { name: "C#", value: 80 },
+    { name: "Cloud Architecture Design", value: 60 },
+    { name: "Cloud Security", value: 50 },
+    { name: "Kubernetes/Containers", value: 45 },
+    { name: "Terraform", value: 50 },
+    { name: "CI/CD", value: 45 },
   ],
   softSkills: [
     { name: "Goal-Oriented", value: 80 },
     { name: "Collaboration", value: 90 },
-    { name: "Positivity", value: 75 },
+    { name: "Positivity", value: 80 },
     { name: "Adaptability", value: 85 },
     { name: "Problem Solving", value: 75 },
     { name: "Empathy", value: 90 },
-    { name: "Organization", value: 70 },
     { name: "Creativity", value: 90 },
   ],
 };
@@ -131,18 +127,18 @@ const getInTouch = {
 };
 
 const experiences = {
-  show: false,
+  show: true,
   heading: "Experiences",
   data: [
     {
       role: 'Cloud Engineer',// Here Add Company Name
-      companylogo: require('../assets/img/dell.png'),
-      date: 'June 2018 – Present',
+      companylogo: require('../assets/img/lazada.jpg'),
+      date: 'September 2023 – Present',
     },
     {
       role: 'Cloud Engineer',
-      companylogo: require('../assets/img/boeing.png'),
-      date: 'May 2017 – May 2018',
+      companylogo: require('../assets/img/ecv.png'),
+      date: 'January 2022 – August 2023',
     },
   ]
 }
